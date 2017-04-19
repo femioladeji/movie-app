f72c28db18fb515923c661de3dc1386f5bc12948
Femi Oladeji <femidotexe@gmail.com>
2017-04-19 16:30:11 +0100

    Created add schedule component

--- src/app/movie-app.module.ts
+++ src/app/movie-app.module.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 import { MovieAppComponent } from './movie-app.component';
 import { MovieList } from './movie-list/movie-list.component';
 import { MovieDetail } from './movie-detail/movie-detail.component';
+import { AddSchedule } from './schedule/add-schedule.component';
 import { TimeTransform } from './shared/time.pipe';
 
 @NgModule({
@@ -15,13 +16,15 @@ import { TimeTransform } from './shared/time.pipe';
         HttpModule,
         RouterModule.forRoot([
             { path: '', component: MovieList },
-            { path: 'movie/:id', component: MovieDetail }
+            { path: 'movie/:id', component: MovieDetail },
+            { path: 'schedule', component: AddSchedule }
         ])
     ],
     declarations: [
         MovieAppComponent,
         MovieList,
         MovieDetail,
+        AddSchedule,
         TimeTransform
     ],
     bootstrap: [ MovieAppComponent ]
--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { MovieService } from '../shared/movie-service';
 import { IAMovie } from '../shared/movie-interface';
+import { SharedService } from '../shared/sharedService';
+
 
 @Component({
     moduleId: module.id,
     templateUrl: './movie-detail.component.html',
-    styleUrls: ['movie-detail.component.css']
+    styleUrls: ['movie-detail.component.css'],
+    providers: [SharedService]
 })
 export class MovieDetail implements OnInit {
     aMovie: IAMovie;
     imdbid: string;
     loading: boolean = true;
 
-    constructor(private _activeRoute: ActivatedRoute, private _movieService: MovieService) {
+    constructor(private _activeRoute: ActivatedRoute,
+        private _movieService: MovieService,
+        private _router: Router,
+        private _sharedService: SharedService) {
 
     }
 
@@ -31,5 +38,11 @@ export class MovieDetail implements OnInit {
             this.loading = false;
             console.log(error);
         })
+    }
+
+    addSchedule() {
+        console.log('added');
+        this._sharedService.scheduleEvent.emit(this.aMovie);
+        this._router.navigate(['/schedule']);
     }
 }
