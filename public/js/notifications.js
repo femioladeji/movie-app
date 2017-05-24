ed515f7ac0ad4bd4f0409e4fb3a92136bb25654d
Femi Oladeji <femidotexe@gmail.com>
2017-05-24 12:57:19 +0100

    Implement action for notification

--- public/js/notifications.js
+++ public/js/notifications.js
@@ -1,11 +1,30 @@
+let notificationId, imdbId;
 chrome.alarms.onAlarm.addListener(data => {
+  console.log(data);
   if (data.name.includes('movie-alarm')) {
     const movieDetails = data.name.split('@#');
+    imdbId = movieDetails[2];
     chrome.notifications.create({
       type: "basic",
       title: "MOVIE TIME",
       message: `Time has come to relax with ${movieDetails[1]}`,
-      iconUrl: "img/logo.png"
-    });
+      iconUrl: "img/logo.png",
+      buttons: [{
+        title: "Watch",
+        iconUrl: "img/theater.png"
+      }]
+      }, (id) => {
+        notificationId = id;
+      });
+  }
+});
+
+chrome.notifications.onButtonClicked.addListener(function(notifId, btnIdx) {
+  if (notifId === notificationId) {
+    if (btnIdx === 0) {
+      // open the streaming url
+      chrome.tabs.create({url: "http://gomovies.to"});
+    }
+    // there is a plan to handle snoozing
   }
 });
